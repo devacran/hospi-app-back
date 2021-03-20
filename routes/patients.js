e5673520ae208bf148cadab5e4cb3ad48a07a483
Miguel Angel de la Rosa Guerrero <migueeldrg@gmail.com>
2021-03-20 10:21:55 -0600

    delete .env

--- routes/patients.js
+++ routes/patients.js
@@ -30,67 +30,69 @@ function patientsApi(app) {
     try {
       const { patientId } = req.params;
       const data = await patientsService.getVitalSigns(patientId);
-      
-      res.status(200).json(data.map(x=>{
-        const {id, patient_id, ...values} = x
-        return {
-          id,
-          patient_id,
-          data: values
-        }
-      }));
+
+      res.status(200).json(
+        data.map((x) => {
+          const { id, patient_id, ...values } = x;
+          return {
+            id,
+            patient_id,
+            data: values,
+          };
+        })
+      );
     } catch (e) {
       console.log(e);
     }
   });
 
-  router.post("/:patientId/vital-signs", async (req, res)=>{
+  router.post("/:patientId/vital-signs", async (req, res) => {
     const reqData = {
-        patientId: req.params.patientId,
-        glucoseLevel: req.query.glucose_level,
-        temp: req.query.temp,
-        heartRate: req.query.heart_rate,
-        bloodPressureS: req.query.blood_pressureS,
-        bloodPressureD: req.query.blood_pressureD,
+      patientId: req.params.patientId,
+      glucoseLevel: req.query.glucose_level,
+      temp: req.query.temp,
+      heartRate: req.query.heart_rate,
+      bloodPressureS: req.query.blood_pressureS,
+      bloodPressureD: req.query.blood_pressureD,
+    };
+    const data = await patientsService.createVitalSigns(reqData);
+    if (data) {
+      res.status(200).json({ id: data.insertId, status: "OK" });
+    } else {
+      res.status(400).json({ status: "ERROR" });
     }
-    const data = await patientsService.createVitalSigns(reqData)
-    if(data){
-      res.status(200).json({id: data.insertId, status: "OK"})
-    }else{
-       res.status(400).json({status: "ERROR"})
-    }
-  })
+  });
 
-  router.delete("/:patientId/vital-signs", async (req, res)=>{
+  router.delete("/:patientId/vital-signs", async (req, res) => {
     const reqData = {
-        patientId: req.params.patientId,
-        id: req.query.id
+      patientId: req.params.patientId,
+      id: req.query.id,
+    };
+    const data = await patientsService.deleteVitalSigns(reqData);
+    if (data) {
+      res.status(200).json({ id: data.insertId, status: "Delete OK!" });
+    } else {
+      res.status(400).json({ status: "ERROR" });
     }
-    const data = await patientsService.deleteVitalSigns(reqData)
-    if(data){
-      res.status(200).json({id: data.insertId, status: "Delete OK!"})
-    }else{
-       res.status(400).json({status: "ERROR"})
+  });
+  router.put("/:patientId/vital-signs", async (req, res) => {
+    const reqData = {
+      patientId: req.params.patientId,
+      id: req.query.id,
+      glucoseLevel: req.query.glucose_level,
+      temp: req.query.temp,
+      heartRate: req.query.heart_rate,
+      bloodPressureS: req.query.blood_pressure_s,
+      bloodPressureD: req.query.blood_pressure_d,
+    };
+    const data = await patientsService.updateVitalSigns(reqData);
+    if (data) {
+      res.status(200).json({ id: data.insertId, status: "Delete OK!" });
+    } else {
+      res.status(400).json({ status: "ERROR" });
     }
-  })
-  router.put("/:patientId/vital-signs", async (req, res)=>{
-   const reqData = {
-        patientId: req.params.patientId,
-        id: req.query.id,
-        glucoseLevel: req.query.glucose_level,
-        temp: req.query.temp,
-        heartRate: req.query.heart_rate,
-        bloodPressureS: req.query.blood_pressure_s,
-        bloodPressureD: req.query.blood_pressure_d,
-    }
-    const data = await patientsService.updateVitalSigns(reqData)
-    if(data){
-      res.status(200).json({id: data.insertId, status: "Delete OK!"})
-    }else{
-       res.status(400).json({status: "ERROR"})
-    }
-  })
-  
+  });
+
   router.get("/:patientId/bill-account", async function (req, res, next) {
     try {
       const { patientId } = req.params;
@@ -101,8 +103,22 @@ function patientsApi(app) {
     }
   });
 
-  
-
+  router.get("/:patientId/prescriptions", async function (req, res, next) {
+    try {
+      const { patientId } = req.params;
+      const reqData = {
+        patientId: req.params.patientId,
+        doctorId: "1",
+        medicineId: req.query.medicine_id,
+        frequency: req.query.frequency,
+        dosis: req.query.dosis,
+      };
+      const patients = await patientsService.getPatientBill(patientId);
+      res.status(200).json({ data: patients });
+    } catch (e) {
+      console.log(e);
+    }
+  });
 
   app.use("/api/patients", router);
 }
